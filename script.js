31f9f759b500811fd8d8983117588c522fb7df65
Kush Gandhi <Kushgandhi2099@gmail.com>
2025-10-08 11:31:54 -0500

    complete challenge 14

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-function colorText() {
-    const text = document.getElementById("p1");
-    text.style.color = "blue";
-    text.style.fontSize = "54px";
-}
-
-function resetText() {
-    const text = document.getElementById("p1");
-    text.style.color = "black";
-    text.style.fontSize = "20px";
+function addListItem() {
+    const list = document.getElementById("list");
+    const newItem = document.createElement("li");
+    newItem.textContent = "New Item";
+    list.appendChild(newItem);
 }
