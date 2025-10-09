2863ec3292ee365b61bd4f604e3701be67633ffe
Kush Gandhi <Kushgandhi2099@gmail.com>
2025-10-09 14:15:25 -0500

    complete challenge 13

---
+++ challenge13/script.js
@@ -0,0 +1,11 @@
+function colorText() {
+    const text = document.getElementById("p1");
+    text.style.color = "blue";
+    text.style.fontSize = "54px";
+}
+
+function resetText() {
+    const text = document.getElementById("p1");
+    text.style.color = "black";
+    text.style.fontSize = "20px";
+}
